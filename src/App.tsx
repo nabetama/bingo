cd3bc09bfaf2a939c862fc996caa411565e96984
nabetama <mao.nabeta@gmail.com>
2022-07-29 03:10:53 +0900

    updated title

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <header></header>
       <main className="text-center">
         <h1 className="text-3xl font-bold underline mb-4 text-rose-700">
-          経営会議用語 BINGO
+          会議用語 BINGO
         </h1>
         <div className="h-56 grid grid-cols-1 gap-1 content-around text-center text-sky-900">
           <Blocks />
