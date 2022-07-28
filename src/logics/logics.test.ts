c9ab260c5dbe51f4864818aaccfd4974416d5dde
nabetama <mao.nabeta@gmail.com>
2022-07-28 23:57:29 +0900

    add test case when bingo

--- src/logics/logics.test.ts
+++ src/logics/logics.test.ts
@@ -8,9 +8,21 @@ describe("isRowBingo()", () => {
     expect(isRowBingo(row)).toBe(true);
   });
 
-  test("not bingo", () => {
+  test("not bingo when all cells are not open", () => {
     const cell = { isOpen: false, label: "dummy" };
     const row = [...Array(5)].map(() => cell);
+
+    expect(isRowBingo(row)).toBe(false);
+  });
+
+  test("not bingo when some cell is not open", () => {
+    const row = [
+      { isOpen: false, label: "dummy" },
+      { isOpen: true, label: "dummy" },
+      { isOpen: false, label: "dummy" },
+      { isOpen: true, label: "dummy" },
+      { isOpen: false, label: "dummy" },
+    ];
 
     expect(isRowBingo(row)).toBe(false);
   });
