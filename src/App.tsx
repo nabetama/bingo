01c397a3c99d1518631937cb16efd2e34e75693a
nabetama <mao.nabeta@gmail.com>
2022-07-28 20:27:15 +0900

    displayed blocks

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,21 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import { BLOCKS } from "./data/blocks";
+import { Block } from "./Block";
+
 function App() {
   return (
     <div className="App">
       <header></header>
+      <main>
+        <h1>BINGO</h1>
+        {BLOCKS.map((labels) => {
+          return labels.map((label) => {
+            return <Block label={label} />;
+          });
+        })}
+      </main>
     </div>
   );
 }
