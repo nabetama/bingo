01f70eeae40e77f24f4b1553ad2345cb590ab005
nabetama <mao.nabeta@gmail.com>
2022-07-29 00:04:26 +0900

    add func isColumnBingo()

--- src/logics/logics.test.ts
+++ src/logics/logics.test.ts
@@ -1,7 +1,8 @@
-import { isRowBingo } from "./logics";
+import exp from "constants";
+import { isColumnBingo, isRowBingo } from "./logics";
 
 describe("isRowBingo()", () => {
-  test("bingo", () => {
+  test("is Bingo", () => {
     const cell = { isOpen: true, label: "dummy" };
     const row = [...Array(5)].map(() => cell);
 
@@ -25,5 +26,37 @@ describe("isRowBingo()", () => {
     ];
 
     expect(isRowBingo(row)).toBe(false);
+  });
+});
+
+describe("isColumnBingo", () => {
+  const state = [
+    [
+      { isOpen: true, label: "1-1" },
+      { isOpen: false, label: "dummy" },
+      { isOpen: false, label: "dummy" },
+    ],
+    [
+      { isOpen: true, label: "2-1" },
+      { isOpen: true, label: "dummy" },
+      { isOpen: false, label: "dummy" },
+    ],
+    [
+      { isOpen: true, label: "3-1" },
+      { isOpen: true, label: "dummy" },
+      { isOpen: false, label: "dummy" },
+    ],
+  ];
+
+  test("is Bingo", () => {
+    expect(isColumnBingo(state, 0)).toBe(true);
+  });
+
+  test("is not bingo when all cells are not open", () => {
+    expect(isColumnBingo(state, 2)).toBe(false);
+  });
+
+  test("not bingo when some cell is not open", () => {
+    expect(isColumnBingo(state, 1)).toBe(false);
   });
 });
--- src/logics/logics.ts
+++ src/logics/logics.ts
@@ -1,5 +1,19 @@
-const isRowBingo = (row: { isOpen: boolean; label: string }[]) => {
+const isRowBingo = (row: { isOpen: boolean; label: string }[]): boolean => {
   return row.every((cell) => cell.isOpen === true);
 };
 
-export { isRowBingo };
+const isColumnBingo = (
+  columnBlocks: { isOpen: boolean; label: string }[][],
+  columnIndex: number
+): boolean => {
+  const arr = columnBlocks.map((blocks) => blocks[columnIndex]);
+
+  console.error(arr);
+
+  if (isRowBingo(arr)) {
+    return true;
+  }
+  return false;
+};
+
+export { isRowBingo, isColumnBingo };
