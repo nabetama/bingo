d676158e35d75181020ad92c93ef54cf9562afb7
nabetama <mao.nabeta@gmail.com>
2022-07-29 03:03:46 +0900

    update

--- src/Congratulations.tsx
+++ src/Congratulations.tsx
@@ -1,11 +1,18 @@
+import MarkovChain from "@hideokamoto/markov-chain-tiny";
 import React from "react";
 
-const Congratulations: React.FC<{ isBingo: boolean }> = (props) => {
-  return (
-    <div className={`${props.isBingo ? "px-8" : "hidden"}`}>
-      Congratulations!
-    </div>
-  );
+const Congratulations: React.FC<{
+  isBingo: boolean;
+}> = (props) => {
+  if (props.isBingo) {
+    return (
+      <div className={`${props.isBingo ? "px-8" : "hidden"}`}>
+        Congratulations!!
+      </div>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export { Congratulations };
