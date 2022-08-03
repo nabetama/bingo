3abfada5a72fc4f91f21e9885e912ccf7fcd3aff
nabetama <mao.nabeta@gmail.com>
2022-08-03 23:59:22 +0900

    add snapshot test

--- src/__tests__/Blocks.test.tsx
+++ src/__tests__/Blocks.test.tsx
@@ -1,14 +1,44 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
 
 import { Block } from "../Block";
 
 describe("<Block />", () => {
   test("renders block with argument value", () => {
-    render(<Block label="dummy" isOpen={false} toggleIsOpen={jest.fn()} />);
-    const val = screen.getByText(/dummy/);
-    expect(val).toBeInTheDocument();
+    const spyToggleIsOpen = jest.fn();
+    const { getByText } = render(
+      <Block label="dummy" isOpen={false} toggleIsOpen={spyToggleIsOpen} />
+    );
+
+    expect(getByText(/dummy/)).toBeInTheDocument();
+    expect(getByText(/dummy/)).toHaveClass("bg-indigo-50");
   });
 
-  test("snapshot", () => {});
+  test("publication action when clicked component", () => {
+    const spyToggleIsOpen = jest.fn();
+    const { getByText } = render(
+      <Block label="dummy" isOpen={false} toggleIsOpen={spyToggleIsOpen} />
+    );
+
+    fireEvent.click(getByText(/dummy/));
+    expect(spyToggleIsOpen).toBeCalledWith({ type: "TOGGLE", label: "dummy" });
+  });
+
+  test("background color has changed when Block called with `isOpen`", () => {
+    const spyToggleIsOpen = jest.fn();
+    const { getByText } = render(
+      <Block label="dummy" isOpen={true} toggleIsOpen={spyToggleIsOpen} />
+    );
+
+    expect(getByText(/dummy/)).toHaveClass("bg-emerald-500");
+  });
+
+  test("snapshot", () => {
+    const component = renderer.create(
+      <Block label="dummy" isOpen={false} toggleIsOpen={jest.fn()} />
+    );
+
+    expect(component).toMatchSnapshot();
+  });
 });
